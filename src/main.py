1435fdf84026963f5a891c0216643c8c9729a5ea
kaibas01 <kaibas01@allegheny.edu>
2024-01-20 14:55:24 -0500

    printing only one article

--- src/main.py
+++ src/main.py
@@ -3,9 +3,14 @@ import newspaper
 #this is to test if the program is working with websites
 def main():
     telex_news = newspaper.build('http://telex.hu')
+    count = 0
     for article in telex_news.articles:
-        print(article.url)
-
+        if article in telex_news.articles:
+            count =+ 1 
+            if count >= 1:
+                firstArticle = telex_news.articles[0]
+                print(firstArticle.url)
+                break
 
 if __name__ == "__main__":
     main()
