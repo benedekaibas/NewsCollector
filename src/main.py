412cb25ff46ae52aa27a3fbd73b717222d09b808
kaibas01 <kaibas01@allegheny.edu>
2024-01-20 14:50:21 -0500

    added a test code with telex.hu

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from newspaper import Article
+import newspaper
+
+#this is to test if the program is working with websites
 def main():
-    urlTelex = "https://telex.hu/"
-    article = Article(urlTelex)
-    downloadedArticle = article.download()
-    print(downloadedArticle)
+    telex_news = newspaper.build('http://telex.hu')
+    for article in telex_news.articles:
+        print(article.url)
 
 
-    
 if __name__ == "__main__":
     main()
