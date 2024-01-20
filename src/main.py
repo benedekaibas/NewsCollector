fa37366c4a6c8c0fc6b67d13486f7edee8d69ce3
kaibas01 <kaibas01@allegheny.edu>
2024-01-20 00:07:00 -0500

    getting articles

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from newspaper import Article
 def main():
     urlTelex = "https://telex.hu/"
     article = Article(urlTelex)
-    
+    downloadedArticle = article.download()
+    print(downloadedArticle)
 
+
+    
 if __name__ == "__main__":
     main()
