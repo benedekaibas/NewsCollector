5fb80e8b096247bb4175a9bfc1550f0d18aafdea
kaibas01 <kaibas01@allegheny.edu>
2024-01-21 12:06:57 -0500

    count method added to the file

--- src/test.py
+++ src/test.py
@@ -11,11 +11,13 @@ def main():
 #TODO:
 #todays date is only working with us news portals but with hungarian it is just not exceeding today's
 #date but it gives all the news before today's date 
-    telex_news = newspaper.build('https://foxnews.com')
+    telex_news = newspaper.build('https://telex.hu')
+    numberOfArticles = telex_news.size()
+    print(numberOfArticles)
     if freshDate == todaysDate:
         for article in telex_news.articles:
             firstArticle = telex_news.articles[0]
             print(firstArticle.url)
-
+            
 if __name__ == "__main__":
     main()
