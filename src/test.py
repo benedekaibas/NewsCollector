d005b7b3f3c3fa781c76f4e836200a2e3e4718a4
kaibas01 <kaibas01@allegheny.edu>
2024-01-21 12:58:02 -0500

    added description

--- src/test.py
+++ src/test.py
@@ -8,11 +8,13 @@ def main():
     freshDate = date.today()
     print(f"You are reading {todaysDate}'s news")
 
-#TODO:
-#todays date is only working with us news portals but with hungarian it is just not exceeding today's
-#date but it gives all the news before today's date 
+
     cnn_news = newspaper.build('https://cnn.com')
     fox_news = newspaper.build('https://www.foxnews.com/')
+    #with adding memoize_articles it gives back all the articles that are found and not just the ones
+    #which are published after we run the program again 
+    index_news = newspaper.build('https://index.hu/', memoize_articles = False)
+    magyarJelen_news = newspaper.build('https://magyarjelen.hu/')
     #chechk how many articles there are on the website 
     numberOfArticles = cnn_news.size()
     print(numberOfArticles)
@@ -24,5 +26,9 @@ def main():
         for article in fox_news.articles:
             print(article.url)
             break
+        for article in index_news.articles:
+            print(article.url)
+            break
+         
 if __name__ == "__main__":
     main()
