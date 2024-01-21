0863136fd1aa6a4f43c97210db9e13bf5fd65993
kaibas01 <kaibas01@allegheny.edu>
2024-01-20 21:46:37 -0500

    added TODO that needs to be done later

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import newspaper
 from newspaper import Article
+
+#TODO: Add date library
+#TODO: Set the time to todays date
+#TODO: After setting today's day write a function that gets news only that day
+
+
+
 #this is to test if the program is working with websites
 def main():
     telex_news = newspaper.build('http://telex.hu', language = 'hu')
@@ -13,6 +20,8 @@ def main():
                 #newsSummary = article.summary
                 #print(newsSummary)
                 break
+
+
 
 if __name__ == "__main__":
     main()
