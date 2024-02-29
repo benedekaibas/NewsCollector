726b331e25f2acb613cc96565f4587878f9bbfea
kaibas01 <kaibas01@allegheny.edu>
2024-02-29 13:56:26 -0500

    file added

--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
-import newspaper
-from newspaper import Article
-from datetime import date 
-#TODO: Add date library
-#TODO: Set the time to todays date
-#TODO: After setting today's day write a function that gets news only that day
+from download_newspaper import NewsArticleBuilder
 
-
-
-#this is to test if the program is working with websites
-import newspaper
-from newspaper import Article
-from datetime import date 
-
-#this is to test if the program is working with websites
 def main():
-    todaysDate = date.today()
-    freshDate = date.today()
-    print(todaysDate)
-
-    telex_news = newspaper.build('https://cnn.com')
-    if freshDate == todaysDate:
-        for article in telex_news.articles:
-            firstArticle = telex_news.articles[0]
-            print(firstArticle.url)
+    output = NewsArticleBuilder
+    print(output)
 
 if __name__ == "__main__":
     main()
-
