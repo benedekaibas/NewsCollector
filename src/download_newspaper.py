07c54f59fb5480fb0486fefe051272846cf0fcbc
kaibas01 <kaibas01@allegheny.edu>
2024-02-29 13:40:00 -0500

    function updated

--- src/download_newspaper.py
+++ src/download_newspaper.py
@@ -19,11 +19,16 @@ class NewsArticleBuilder:
             convert_telex_url = str(telex_url)
             write = fh.write(convert_telex_url)
             return write
+    def read_file(self):
+        with open(self.file_path, "r") as fh:
+            lines = fh.readlines()
+            return lines
 
-    def format_txt_file(self, write):
-        formatted_txt_file = "\n".join(str(line) for line in write)
-        return formatted_txt_file
-    
+    def split_lines_in_file(self):
+        lines = self.read_file()
+        for line in lines:
+            split = line.split(",")
+            return split
 
 if __name__ == "__main__":
     url = 'http://telex.hu'
@@ -32,5 +37,5 @@ if __name__ == "__main__":
     news_builder = NewsArticleBuilder(url, file_path)
 
     telex_url = news_builder.build()
-    write = news_builder.open_url(telex_url)
-    formatted_txt_file = news_builder.format_txt_file(write)
+    news_builder.open_url(telex_url)
+    news_builder.split_lines_in_file()
